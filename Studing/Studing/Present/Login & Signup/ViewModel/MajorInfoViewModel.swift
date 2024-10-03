4340f0a95d8a23b1f97c7136ed5dff48ae602d5b
Genesis <dmsgktn1685@naver.com>
2024-10-03 20:11:26 +0900

    [Feat] #13 - 학교, 학과 검색 관련 로직 및 UI 구현

--- Studing/Studing/Present/Login & Signup/ViewModel/MajorInfoViewModel.swift
+++ Studing/Studing/Present/Login & Signup/ViewModel/MajorInfoViewModel.swift
@@ -8,17 +8,21 @@
 import Foundation
 import Combine
 
+let majorInfoData: SearchMajorInfoResponseDTO = SearchMajorInfoResponseDTO(data: ["컴퓨터공학과", "컴퓨터시스템공학과", "컴퓨터학부"])
+
 final class MajorInfoViewModel: BaseViewModel {
     
     // MARK: - Input
     
     struct Input {
+        let majorName: AnyPublisher<String, Never>
         let nextTap: AnyPublisher<Void, Never>
     }
     
     // MARK: - Output
     
     struct Output {
+        let searchMajorResult: AnyPublisher<([MajorInfoModel], String), Never>
         let TermsOfServiceViewAction: AnyPublisher<Void, Never>
     }
     
@@ -26,11 +30,56 @@ final class MajorInfoViewModel: BaseViewModel {
     
     private var cancellables = Set<AnyCancellable>()
     
-    
     // MARK: - Public methods
     
     func transform(input: Input) -> Output {
         
-        return Output(TermsOfServiceViewAction: input.nextTap)
+        let majorNamesPublisher = input.majorName
+            .flatMap { [weak self] name -> AnyPublisher<([MajorInfoModel], String), Never> in
+                guard let self = self else {
+                    return Just(([], "")).eraseToAnyPublisher()
+                }
+                
+                return self.postSearchMajorPublisher(with: name)
+                    .map { dto in
+                        let models = dto.data.map { MajorInfoModel(name: $0) }
+                        return (models, name)
+                    }
+                    .catch { _ in Just(([], "")) }
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+        
+        return Output(
+            searchMajorResult: majorNamesPublisher,
+            TermsOfServiceViewAction: input.nextTap
+        )
+    }
+}
+
+private extension MajorInfoViewModel {
+    // TODO: - 학과 이름 검색 API 구현
+    func postSearchMajorPublisher(with name: String) -> AnyPublisher<SearchMajorInfoResponseDTO, Error> {
+            Future { promise in
+                Task {
+                    do {
+                        let result = try await self.search(with: name)
+                        promise(.success(result))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            }
+            .eraseToAnyPublisher()
+        }
+    
+    // TODO: - 실제로는 필터 로직은 없어질 계획, API 붙이기전 로직 확인
+    func search(with name: String) async throws -> SearchMajorInfoResponseDTO {
+        
+        let filteredData = majorInfoData.data.filter { universityInfo in
+            universityInfo.contains(name) // 대학교 이름에 주어진 name이 포함되는지 확인
+        }
+        
+        return SearchMajorInfoResponseDTO(data: filteredData)
     }
 }
--- Studing/Studing/Present/Login & Signup/ViewModel/UniversityInfoViewModel.swift
+++ Studing/Studing/Present/Login & Signup/ViewModel/UniversityInfoViewModel.swift
@@ -8,6 +8,8 @@
 import Foundation
 import Combine
 
+let universityInfoData: SearchUniversityInfoResponseDTO = SearchUniversityInfoResponseDTO(data: ["서울과학기술대", "서울대학교", "서울여자대학교"])
+
 protocol InputUniversityNameDelegate: AnyObject {
     func didSubmitUniversityName(_ name: String)
 }
@@ -19,19 +21,20 @@ final class UniversityInfoViewModel: BaseViewModel {
     // MARK: - Input
     
     struct Input {
+        let universityName: AnyPublisher<String, Never>
         let nextTap: AnyPublisher<Void, Never>
     }
     
     // MARK: - Output
     
     struct Output {
+        let searchUnivsersityResult: AnyPublisher<([UniversityInfoModel], String), Never>
         let majorInfoViewAction: AnyPublisher<Void, Never>
     }
     
     // MARK: - Private properties
     
     private var cancellables = Set<AnyCancellable>()
-    
     
     func submitUniversityName(name: String) {
         // 학교 정보를 처리한 후 delegate에 전달
@@ -42,6 +45,52 @@ final class UniversityInfoViewModel: BaseViewModel {
     
     func transform(input: Input) -> Output {
         
-        return Output(majorInfoViewAction: input.nextTap)
+        let universityNamesPublisher = input.universityName
+            .flatMap { [weak self] name -> AnyPublisher<([UniversityInfoModel], String), Never> in
+                guard let self = self else {
+                    return Just(([], "")).eraseToAnyPublisher()
+                }
+                
+                return self.postSearchUniversityPublisher(with: name)
+                    .map { dto in
+                        let models = dto.data.map { UniversityInfoModel(name: $0) }
+                        return (models, name)
+                    }
+                    .catch { _ in Just(([], "")) }
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+        
+        return Output(
+            searchUnivsersityResult: universityNamesPublisher,
+            majorInfoViewAction: input.nextTap
+        )
+    }
+}
+
+private extension UniversityInfoViewModel {
+    // TODO: - 대학교 이름 검색 API 구현
+    func postSearchUniversityPublisher(with name: String) -> AnyPublisher<SearchUniversityInfoResponseDTO, Error> {
+            Future { promise in
+                Task {
+                    do {
+                        let result = try await self.search(with: name)
+                        promise(.success(result))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            }
+            .eraseToAnyPublisher()
+        }
+    
+    // TODO: - 실제로는 필터 로직은 없어질 계획, API 붙이기전 로직 확인
+    func search(with name: String) async throws -> SearchUniversityInfoResponseDTO {
+        
+        let filteredData = universityInfoData.data.filter { universityInfo in
+            universityInfo.contains(name) // 대학교 이름에 주어진 name이 포함되는지 확인
+        }
+        
+        return SearchUniversityInfoResponseDTO(data: filteredData)
     }
 }
