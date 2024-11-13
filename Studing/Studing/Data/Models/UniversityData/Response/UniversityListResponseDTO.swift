5c0d2dc8d0d62d7b5bcc51454b4afd4633f10402
Genesis <dmsgktn1685@naver.com>
2024-11-13 22:46:32 +0900

    [Feat] #35 - UniversityData 관련 Request & Response & Entity 구현

---
+++ Studing/Studing/Data/Models/UniversityData/Response/UniversityListResponseDTO.swift
@@ -0,0 +1,10 @@
+//
+//  UniversityListResponseDTO.swift
+//  Studing
+//
+//  Created by ParkJunHyuk on 11/11/24.
+//
+
+import Foundation
+
+typealias UniversityListResponse = APIResponse<[String]>
